06cfe0484b12e78deaf1315dbe5716389156f411
Yoshihiro Tanaka <contact@cordea.jp>
2021-01-31 16:54:35 +0900

    find the live chat id

--- api_client.py
+++ api_client.py
@@ -9,6 +9,11 @@ class ApiClient:
             developerKey=key
         )
 
+    def find_chat_id(self, video_id):
+        request = self.client.videos().list(id=video_id, part='id,snippet,liveStreamingDetails')
+        response = request.execute()
+        return response['items'][0]['liveStreamingDetails']['activeLiveChatId']
+
     def fetch_messages(self, id):
         messages = self.client.liveChatMessages()
         request = messages.list(liveChatId=id, part='snippet')
